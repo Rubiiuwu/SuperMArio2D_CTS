0e50145a379064b02a32a32fb9416d681cad648e
Rubiiuwu <andreacorteszamora@gmail.com>
2023-01-18 10:53:34 +0100

    SuperMario2D_AndreaCortés

---
+++ Assets/scripts/GroundSensor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+   public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+   {
+        isGrounded = true; 
+   }
+
+void OnTriggerExit2D(Collider2D other)
+   {
+        isGrounded = false; 
+   }
+
+}
--- Assets/scripts/PLAYER_CONTROLER.cs
+++ Assets/scripts/PLAYER_CONTROLER.cs
@@ -6,12 +6,22 @@ public class PLAYER_CONTROLER : MonoBehaviour
 {
     int playerhealth = 3; 
     public float playerspeed = 5.5f;
+    public float jumpForce = 3f;
+
     string texto = "hello world";
-    bool isGrounded = false;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rBody;
+    private GroundSensor sensor;
+    float horizontal;
 
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent <SpriteRenderer> (); 
+        rBody = GetComponent <Rigidbody2D>();
+        sensor = GameObject.Find("GroundSensor") .GetComponent<GroundSensor>();
+
         playerhealth = 10; 
         Debug.Log(texto);
     }
@@ -19,6 +29,22 @@ public class PLAYER_CONTROLER : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3 (Input.GetAxis ("Horizontal"),0,0)* playerspeed * Time.deltaTime;
+        horizontal = Input.GetAxis ("Horizontal") ;
+
+        transform.position += new Vector3 (horizontal,0,0)* playerspeed * Time.deltaTime;
+
+        if (horizontal < 0)
+        {
+            spriteRenderer .flipX = true;
+        } else if (horizontal>0) 
+        {
+            spriteRenderer .flipX = false;
+        }
+       
+       if(Input.GetButtonDown ("Jump") && sensor.isGrounded)
+       {
+         rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
     }
-}
+} 
